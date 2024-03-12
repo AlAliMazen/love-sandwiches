842efb782482a9ddd8a1d1c0b313dfa61e762b36
Mazen Al Ali <al.ali.mazen@outlook.com>
2024-03-12 17:47:41 +0100

    get user input, and validate it in length

--- run.py
+++ run.py
@@ -39,3 +39,34 @@ for r in range(len(data)):
     for c in range(len(data[r])):
         print(f'Row: {r} Col{c} : {data[r][c]}')
 """
+
+def get_sales_data():
+    """
+    Get sales figues input from the user
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six number, separated by commas.")
+    print("Examples: 120,20,30,40,50,60\n")
+
+    data_str=input("Enter your data here: ")
+
+    #spit the data and put them into a list using split on string
+    sales_data = data_str.split(",")
+
+    #call a function to check if the length is exactly 6 and all can be converted into integers
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    validate the values inside the list whether they are exactly 6 and can be converted into integers
+    """
+    try:
+        if len(values) !=6:
+            raise ValueError(
+                 f'Exactly 6 figures required. You provided {len(values)}'
+                )
+    except ValueError as e:
+        print(f'Invalid input: {e}, please try again')
+
+
+get_sales_data()
